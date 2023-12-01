f13476edf2c779e6e530ebe5af9a0362a149052e
colemcarthur <92132630+colemcarthur@users.noreply.github.com>
2023-11-30 19:29:21 -0700

    Removed background photo on shop page

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -7,10 +7,7 @@ export default async function Home() {
 
   return (
     <main>
-      <div style={{
-        backgroundImage: 'url("/img/pepper_background_2.webp")'
-      }}
-        className="top-0 left-0 w-full h-full flex justify-center bg-cover bg-center">
+      <div className="top-0 left-0 w-full h-full flex justify-center bg-cover bg-center">
         <div className="py-6 bg-center w-[80rem] bg-opacity-75 flex flex-wrap gap-4 justify-between">
           {peppers.map(pepper => (<PepperCard key={pepper.id} {...pepper} />))}
         </div>
